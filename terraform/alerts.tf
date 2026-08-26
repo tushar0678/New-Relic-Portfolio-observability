4ea3d152935ddfd6148c7437bc11241b8f650cfc
Tushar Shukla <123179380+tushar0678@users.noreply.github.com>
2026-08-26 23:30:08 +0530

    Add New Relic alert conditions

---
+++ terraform/alerts.tf
@@ -0,0 +1,50 @@
+resource "newrelic_alert_policy" "portfolio_policy" {
+  name                = "${local.site} - Observability"
+  incident_preference = "PER_CONDITION"
+}
+
+resource "newrelic_nrql_alert_condition" "browser_failed" {
+  policy_id = newrelic_alert_policy.portfolio_policy.id
+  name      = "${local.homepage_monitor} Failed"
+  type      = "static"
+  enabled   = true
+
+  nrql {
+    query = "SELECT count(*) FROM SyntheticCheck WHERE monitorName = '${local.homepage_monitor}' AND result = 'FAILED'"
+  }
+
+  warning {
+    operator              = "above_or_equals"
+    threshold             = 1
+    threshold_duration    = 60
+    threshold_occurrences = "ALL"
+  }
+
+  aggregation_window             = 60
+  aggregation_method             = "event_flow"
+  aggregation_delay              = 30
+  expiration_duration            = 3600
+  close_violations_on_expiration = true
+}
+
+resource "newrelic_nrql_alert_condition" "ping_failed" {
+  policy_id = newrelic_alert_policy.portfolio_policy.id
+  name      = "${local.ping_monitor} Failed"
+  type      = "static"
+  enabled   = true
+
+  nrql {
+    query = "SELECT count(*) FROM SyntheticCheck WHERE monitorName = '${local.ping_monitor}' AND result != 'SUCCESS'"
+  }
+
+  warning {
+    operator              = "above_or_equals"
+    threshold             = 1
+    threshold_duration    = 300
+    threshold_occurrences = "AT_LEAST_ONCE"
+  }
+
+  aggregation_window = 300
+  aggregation_method = "event_flow"
+  aggregation_delay  = 60
+}
