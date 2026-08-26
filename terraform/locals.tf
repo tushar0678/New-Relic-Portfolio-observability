286d53855d86c439a214e5c33da5ef196ab62b2a
Tushar Shukla <123179380+tushar0678@users.noreply.github.com>
2026-08-26 23:34:26 +0530

    Rename portfolio observability placeholders

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,11 @@
 locals {
-  site             = "Tushar Portfolio"
-  homepage_monitor = "${local.site} - Portfolio Browser"
-  ping_monitor     = "${local.site} - Portfolio Ping"
-  locations_public = ["AP_EAST_1"]
-  homepage_url     = "https://tushar0678.github.io/"
+  site                  = "Tushar Shukla Portfolio"
+  homepage_monitor      = "${local.site} - Homepage Browser"
+  availability_monitor  = "${local.site} - Website Availability"
+  locations_public      = ["AP_EAST_1"]
+  homepage_url          = "https://tushar0678.github.io/"
+  browser_alert_name    = "${local.site} - Homepage Browser Failure"
+  availability_alert    = "${local.site} - Website Availability Failure"
+  workflow_name         = "${local.site} - Observability Workflow"
+  enrichment_name       = "${local.site} - Homepage Error Enrichment"
 }
