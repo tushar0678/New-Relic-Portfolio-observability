6b7d6d0cdb48f2df6998c0f31e22a6aa284bce5e
Tushar Shukla <123179380+tushar0678@users.noreply.github.com>
2026-08-26 23:30:14 +0530

    Add portfolio monitoring locals

---
+++ terraform/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  site             = "Tushar Portfolio"
+  homepage_monitor = "${local.site} - Portfolio Browser"
+  ping_monitor     = "${local.site} - Portfolio Ping"
+  locations_public = ["AP_EAST_1"]
+  homepage_url     = "https://tushar0678.github.io/"
+}
